32ee02e52b7da58a58ac30dd1d8b55b4c61bcdeb
yousef_ayman <yousefaymanelsayed@gmail.com>
2021-09-22 15:05:06 +0200

    five commit

--- src/pages/AddURL/index.js
+++ src/pages/AddURL/index.js
@@ -2,31 +2,27 @@ import React, { useEffect, useState } from 'react'
 import Logo from "../../logo.png"
 
 export default function Index() {
-    const [link, setLink] = useState('');
+    const [link, setLink] = useState('https://afakyerp.com/');
     const [links, setLinks] = useState([]);
-
+console.log(link);
     useEffect(() => {
         if( localStorage.getItem('linkAfaky') ) {
             let getLinks = JSON.parse(localStorage.getItem('linkAfaky'));
-            window.location.replace(getLinks)
-        }else {
-            setTimeout(() => {
-                window.location.replace('https://afakyerp.com/')
-            }, 5000);
+            setLink(getLinks)
         }
+        // else {
+        //     setTimeout(() => {
+        //         window.location.replace('https://afakyerp.com/')
+        //     }, 5000);
+        // }
     }, [])
 
-    // const saveData = link => {
-    //     let filterLinks = links.find(item => item == link);
-    //     if(!filterLinks){
-    //         links.push(link);
-    //         localStorage.setItem('linkAfaky', JSON.stringify(links))         
-    //     }
-    // }
 
     const saveData = () => {
         // console.log(link);
-        localStorage.setItem('linkAfaky', JSON.stringify(link));
+        if(link !== 'https://afakyerp.com/'){
+            localStorage.setItem('linkAfaky', JSON.stringify(link));
+        }
     }
 
     return (
@@ -34,26 +30,18 @@ export default function Index() {
             <div className="row d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
                 <div className="col-12 text-center">
                     <img src={Logo} className="mt-4" />
-                    <input type="text" className="form-control mt-5 mb-3" onChange={e => setLink(e.target.value) } placeholder="Add Your URL" />
+                    <input type="text" className="form-control mt-5 mb-3" value={link} onChange={e => setLink(e.target.value) } placeholder="Add Your URL" />
                     {/* <a href={link} className="btn btn-outline-danger" onClick={() => saveData()}> Go To Link </a> */}
-                    <a href={link} className="btn btn-site d-block" onClick={() => saveData()}> Go To Link </a>
+                    {/* <a href={link} className="btn btn-site d-block" onClick={() => saveData()}> Go To Link </a> */}
+                    
+                    {link === '' ? 
+                        <button className="btn btn-site d-block w-100" disabled> Go To Link </button>
+                    : 
+                        <a href={link} className="btn btn-site d-block" onClick={() => saveData()}> Go To Link </a>
+                    }
                     {/* <a href="#" className="btn btn-outline-danger" onClick={() => saveData(link)}> Go To Link </a> */}
                 </div>
-                <div className="col-12 mx-auto">
-                <div className="list-group">
-                    {links.map(( item, i ) => {
-                        return (
-                            <div className="item bg-light d-flex justify-content-between my-2" key={i}>
-                                <a href={item} className="list-group-item list-group-item-action"> 
-                                    <span> { item } </span>
-                                </a>
-                                <button className="btn btn-danger py-1"> Remove </button>
-                            </div>
-                            
-                        )
-                    })}
-                    </div>
-                </div>
+
             </div>
         </div>
     )
